cbdceb9f100733081f799d527e08f27255ca0893
Vansh Saini <macbook-air@Vanshs-MacBook-Air.local>
2026-06-29 11:02:28 +0530

    added projects section

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { Suspense, useState } from "react";
 import { myProjects } from "../constants";
+import { Canvas } from "@react-three/fiber";
+import { Center, OrbitControls } from "@react-three/drei";
+import CanvasLoader from "../components/CanvasLoader";
+import DemoComputer from "../components/DemoComputer";
 
+const projectCount = myProjects.length;
 const Projects = () => {
-  const currentProject = myProjects[0];
+  const [selectedProjectIndex, setSelectedProjectIndex] = useState(0);
+  const currentProject = myProjects[selectedProjectIndex];
+  const handleNavigation = (direction) => {
+    setSelectedProjectIndex((prevIndex) => {
+      if (direction === "previous") {
+        return prevIndex === 0 ? projectCount - 1 : prevIndex - 1;
+      } else {
+        return prevIndex === projectCount - 1 ? 0 : prevIndex + 1;
+      }
+    });
+  };
   return (
     <section className="c-space my-20">
       <p className="head-text ">My Work</p>
@@ -32,6 +47,61 @@ const Projects = () => {
             <p className="animatedText">{currentProject.desc}</p>
             <p className="animatedText">{currentProject.subdesc}</p>
           </div>
+          <div className="flex items-center justify-between flex-wrap gap-5">
+            <div className="flex items-center gap-3">
+              {currentProject.tags.map((tag, index) => (
+                <div key={index} className="tech-logo">
+                  <img src={tag.path} alt="tag.name" />
+                </div>
+              ))}
+            </div>
+            <a
+              href={currentProject.href}
+              className="flex items-center gap-2 cursor-pointer text-white-600"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <p>Check Live Site</p>
+              <img src="assets/arrow-up.png" alt="arrow" className="w-3 h-3" />
+            </a>
+          </div>
+          <div className="flex justify-between items-center mt-7">
+            <button
+              className="arrow-btn"
+              onClick={() => handleNavigation("previous")}
+            >
+              <img
+                src="/assets/left-arrow.png"
+                alt=" right arrow"
+                className="w-4 h-4"
+              />
+            </button>
+            <button
+              className="arrow-btn"
+              onClick={() => handleNavigation("next")}
+            >
+              <img
+                src="/assets/right-arrow.png"
+                alt=" right arrow"
+                className="w-4 h-4"
+              />
+            </button>
+          </div>
+        </div>
+
+        <div className="border border-black-300 bg-black-200 rounded-lg h-96 md:h-full">
+          <Canvas>
+            <ambientLight intensity={Math.PI} />
+            <directionalLight position={[10, 10, 5]} />
+            <Center>
+              <Suspense fallback={<CanvasLoader />}>
+                <group scale={2} position={[-0.42, -2.8, -0.3]} rotation={[0, -0.1, 0]}>
+                  <DemoComputer texture={currentProject.texture} scale={1}  />
+                </group>
+              </Suspense>
+            </Center>
+            <OrbitControls maxPolarAngle={Math.PI / 2} enableZoom={false} />
+          </Canvas>
         </div>
       </div>
     </section>
